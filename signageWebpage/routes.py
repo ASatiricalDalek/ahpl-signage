fad4c7911d086a51ced4c59ef75733ceb06c01fd
Connor McNamara <dalek124@gmail.com>
2019-10-21 13:35:07 -0400

    10/21/19 - CSS created

--- signageWebpage/routes.py
+++ signageWebpage/routes.py
@@ -3,11 +3,26 @@ from flask import render_template
 import scrape
 
 
-@sw.route("/")
-@sw.route("/index")
-def index():
+@sw.route("/storytime")
+def storytime():
     room = "Storytime Room"
     date = scrape.get_assabet_date()
     events = scrape.get_events_now(room)
     # TODO: Don't display canceled events
-    return render_template("cmr.html", room=room, today=date, events=events)
+    return render_template("events.html", room=room, today=date, events=events)
+
+
+@sw.route("/community")
+def community():
+    room = "Community Meeting Room"
+    date = scrape.get_assabet_date()
+    events = scrape.get_events_now(room)
+    return render_template("events.html", room=room, today=date, events=events)
+
+
+@sw.route("/small")
+def small():
+    room = "Small Meeting Room"
+    date = scrape.get_assabet_date()
+    events = scrape.get_events_now(room)
+    return render_template("events.html", room=room, today=date, events=events)
