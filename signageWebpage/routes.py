c646031087d07e10524a3c64214165238680eb11
Connor McNamara <dalek124@gmail.com>
2019-11-27 17:54:12 -0500

    11/27/2019 - v1.5 Preview: added wayfinding sign feat.

--- signageWebpage/routes.py
+++ signageWebpage/routes.py
@@ -41,3 +41,22 @@ def small():
     logging.info("Checking to see if events were found...")
     noEvents = renderLogic.no_events_check(events)
     return render_template("events.html", room=room, today=date, events=events, noEvents=noEvents)
+
+@sw.route("/")
+def all():
+    scr = "Small Conference Room"
+    cr = "Community Room"
+    ar = "Activity Room"
+    logging.info("Getting events for all rooms")
+    date = scrape.get_assabet_date()
+    logging.info("Event date " + str(date))
+    logging.info("Getting events")
+    scrEvents = scrape.get_events_now(scr)
+    crEvents = scrape.get_events_now(cr)
+    arEvents = scrape.get_events_now(ar)
+    logging.info("Checking to see if events were found...")
+    scrNoEvents = renderLogic.no_events_check(scrEvents)
+    crNoEvents = renderLogic.no_events_check(crEvents)
+    arNoEvents = renderLogic.no_events_check(arEvents)
+    return render_template("AllRooms.html", today=date, scrEvents=scrEvents, crEvents=crEvents, arEvents=arEvents,
+                           scrNoEvents=scrNoEvents, arNoEvents=arNoEvents, crNoEvents=crNoEvents)
